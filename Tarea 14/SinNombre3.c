758b4f078422ad9dbea8ab05585810551bbed3fc
raygadasbaezrodrigo <31478555+raygadasbaezrodrigo@users.noreply.github.com>
2017-10-29 18:22:50 -0600

    Add files via upload

---
+++ Tarea 14/SinNombre3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define MAX 5
+
+using namespace std;
+
+int main ()
+{
+	int edad=0;
+	const float PI=3.1415;
+	
+	printf("¿Cual es tu edad\?: ");
+	scanf("%d",&edad);
+	
+	printf("Tu edad es: %d\n",edad);
+	printf("Maximo=%d\n",MAX);
+	printf("Valor de PI=%f\n",PI);
+	
+}
